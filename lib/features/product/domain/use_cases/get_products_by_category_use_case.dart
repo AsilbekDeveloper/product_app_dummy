fe99174f9f8afd163cada910b9aed6be83dce7b1
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-09-06 17:35:18 +0500

    Add Get product by category use case

---
+++ lib/features/product/domain/use_cases/get_products_by_category_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:product_app/features/product/domain/entities/product_entity.dart';
+import 'package:product_app/features/product/domain/repositories/product_repository.dart';
+
+class GetProductsByCategoryUseCase {
+  final ProductRepository productRepository;
+
+  GetProductsByCategoryUseCase({required this.productRepository});
+
+  Future<List<ProductEntity>> call({required String categoryName}) {
+    return productRepository.getProductsByCategory(categoryName: categoryName);
+  }
+}
