bc30aac7d2ab90d37d43a8f1b4f4ad1a73c866c0
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-09-11 02:17:25 +0500

    Implement ProductRepositoryImpl with remote data source integration

---
+++ lib/features/product/data/repositories/product_repository_impl.dart
@@ -0,0 +1,64 @@
+import 'package:product_app/features/product/data/data_source/product_remote_data_source.dart';
+import 'package:product_app/features/product/domain/entities/category_entity.dart';
+import 'package:product_app/features/product/domain/entities/product_entity.dart';
+import 'package:product_app/features/product/domain/entities/sort_types.dart';
+import 'package:product_app/features/product/domain/repositories/product_repository.dart';
+
+class ProductRepositoryImpl extends ProductRepository {
+  final ProductRemoteDataSource remoteDataSource;
+
+  ProductRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<List<ProductEntity>> getAllProducts() {
+    return remoteDataSource.getAllProducts();
+  }
+
+  @override
+  Future<ProductEntity> getSingleProduct({required int id}) {
+    return remoteDataSource.getSingleProduct(id: id);
+  }
+
+  @override
+  Future<List<ProductEntity>> searchProduct({required String productTitle}) {
+    return remoteDataSource.searchProduct(productTitle: productTitle);
+  }
+
+  @override
+  Future<List<ProductEntity>> sortProducts({
+    required SortType sortType,
+    required String order,
+  }) {
+    return remoteDataSource.sortProducts(sortType: sortType, order: order);
+  }
+
+  @override
+  Future<List<CategoryEntity>> getAllCategories() {
+    return remoteDataSource.getAllCategories();
+  }
+
+  @override
+  Future<List<ProductEntity>> getProductsByCategory({
+    required String categoryName,
+  }) {
+    return remoteDataSource.getProductsByCategory(categoryName: categoryName);
+  }
+
+  @override
+  Future<void> addProduct({required ProductEntity product}) {
+    return remoteDataSource.addProduct(product: product);
+  }
+
+  @override
+  Future<void> updateProduct({
+    required int id,
+    required ProductEntity product,
+  }) {
+    return remoteDataSource.updateProduct(id: id, product: product);
+  }
+
+  @override
+  Future<void> deleteProduct({required int id}) {
+    return remoteDataSource.deleteProduct(id: id);
+  }
+}
