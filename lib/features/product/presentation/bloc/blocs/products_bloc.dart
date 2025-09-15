85f1f4aa667829c56c181d9fd1c70238db6d6c5f
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-09-15 21:00:52 +0500

    Remove generated files

---
+++ lib/features/product/presentation/bloc/blocs/products_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:bloc/bloc.dart';
+import 'package:product_app/features/product/presentation/bloc/products_event.dart';
+import 'package:product_app/features/product/presentation/bloc/products_state.dart';
+
+class ProductsBloc extends Bloc<ProductsEvent, ProductsState> {
+  final
+}
