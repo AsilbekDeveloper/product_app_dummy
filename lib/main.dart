6ff87680f5f9b57e3f30aa44a9464e0c8b50d883
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-09-16 17:41:37 +0500

    Integrate DI setup call in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:product_app/app.dart';
+import 'package:product_app/core/di/injection_container.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setup();
   runApp(const MyApp());
 }
 
